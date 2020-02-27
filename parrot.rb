ef563d6f3eb535f9ebe6e2a84df1ab0b0807ac37
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-02-27 19:12:55 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot()
+def parrot(phrase)
   puts "Squawk!"
+  puts phrase
   return "Squawk!"
+  return phrase
 end
